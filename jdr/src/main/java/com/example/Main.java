129c65a56233b25eafedd34e2cb9ef51db94efff
paulPicotin <paul.picotin@gmail.com>
2023-09-19 01:02:36 +0200

    Gestion d'erreur de message d'affichage de la table

--- jdr/src/main/java/com/example/Main.java
+++ jdr/src/main/java/com/example/Main.java
@@ -20,8 +20,7 @@ public class Main {
              player.getInventaire();
             System.out.println("veuillez choisir votre classe");
             String str3 = sc2.nextLine();
-            class1.setClasse(str3);
-            class1.CheckClasse(str3);
+            class1.setClasse(class1.CheckClasse(str3));
             player.setClass1(class1.classe); 
         }       
         System.out.println(player.pseudo);
